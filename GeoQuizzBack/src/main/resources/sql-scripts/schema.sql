0c97cb34cd26f9e3eaddfaf51cd52542d988f6e9
alexandre <alexandre.robert57@gmail.com>
2019-03-16 14:34:39 +0100

    add people DB

---
+++ GeoQuizzBack/src/main/resources/sql-scripts/schema.sql
@@ -0,0 +1,68 @@
+-- noinspection SqlNoDataSourceInspectionForFile
+
+CREATE TABLE serie (
+    PRIMARY KEY (id),
+    id varchar(255) DEFAULT NULL,
+    ville varchar(255) DEFAULT NULL,
+    map_refs varchar(255) DEFAULT NULL,
+    dist bigint(20) DEFAULT NULL,
+);
+CREATE TABLE partie (
+    PRIMARY KEY (partie_id),
+    partie_id varchar(255) DEFAULT NULL,
+    token varchar(255) DEFAULT NULL,
+    status varchar(255) DEFAULT NULL,
+    score bigint(20) DEFAULT NULL,
+    nb_photos bigint(20) DEFAULT NULL,
+    joueur varchar(255) DEFAULT NULL,
+    serie_id varchar(255) DEFAULT NULL,
+    FOREIGN KEY (serie_id) REFERENCES serie(id),
+);
+
+CREATE TABLE photo (
+    PRIMARY KEY (photo_id),
+    photo_id varchar(255) DEFAULT NULL,
+    description varchar(255) DEFAULT NULL,
+    position varchar(255) DEFAULT NULL,
+    url varchar(255) DEFAULT NULL,
+    serie_id varchar(255) DEFAULT NULL,
+    FOREIGN KEY (serie_id) REFERENCES serie(id),
+);
+
+CREATE TABLE pp (
+    partie_id varchar(255) NOT NULL,
+    photo_id varchar(255) NOT NULL,
+    FOREIGN KEY (partie_id) REFERENCES partie (partie_id) ON DELETE RESTRICT ON UPDATE CASCADE,
+    FOREIGN KEY (photo_id) REFERENCES photo (photo_id) ON DELETE RESTRICT ON UPDATE CASCADE,
+    PRIMARY KEY (partie_id, photo_id)
+);
+
+-- CREATE TABLE app_user (
+--     id  varchar(255) DEFAULT NULL,
+--     first_name varchar(255) DEFAULT NULL,
+--     last_name varchar(255) DEFAULT NULL,
+--     password varchar(255) DEFAULT NULL,
+--     username varchar(255) DEFAULT NULL,
+--     PRIMARY KEY (id)
+-- );
+
+
+--
+-- CREATE TABLE sandwich (
+--     PRIMARY KEY (id),
+--     id varchar(255) DEFAULT NULL,
+--     nom varchar(255) DEFAULT NULL,
+--     description varchar(255) DEFAULT NULL,
+--     pain varchar(255) DEFAULT NULL,
+--     prix bigint(20) DEFAULT NULL,
+--     categorie_id varchar(255) DEFAULT NULL,
+--     FOREIGN KEY (categorie_id) REFERENCES categorie(id),
+-- );
+--
+-- CREATE TABLE facture (
+--     PRIMARY KEY (id),
+--     id varchar(255) DEFAULT NULL,
+--     nom varchar(255) DEFAULT NULL,
+--     date_paiement varchar(255) DEFAULT NULL,
+--     montant bigint(20) DEFAULT NULL,
+-- );
